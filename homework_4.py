f8b4bde518a0b92aa36a66658af9d10666ae7fd4
Kevin <kevinm1776@gmail.com>
2020-04-08 14:35:31 -0400

    Fixed code and added test.

--- homework_4.py
+++ homework_4.py
@@ -1,15 +1,17 @@
 def longest_unique_substring_in(s):
-	last_seen_index_of = {}
-	start = 0
-	longest_seen = 0
-	for index, letter in enumerate(s):
-		if letter in last_seen_index_of:
-			start = last_seen_index_of(letter) + 1
+    last_seen_index_of = {}
+    start = 0
+    longest_seen = 0
+    for index, l in enumerate(s):
+        if l in last_seen_index_of and last_seen_index_of[l] >= start:
+            start = last_seen_index_of[l] + 1
 
-		last_seen_index_of(letter) = index
+        last_seen_index_of[l] = index
 
-		if index - start > longest_seen:
-			longest_seen = index - start
+        if index - start > longest_seen:
+            longest_seen = index - start
 
-	return longest_seen
+    return longest_seen + 1
 
+if __name__ == '__main__':
+    print(longest_unique_substring_in('absafionamcopkfa'))
