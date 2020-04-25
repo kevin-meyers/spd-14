b952ebc97c9640c00893303bd276637c72d7c131
Kevin <kevinm1776@gmail.com>
2020-04-25 02:24:48 -0400

    Using maybe type correctly!

--- exercism/haskell/rna-transcription/src/DNA.hs
+++ exercism/haskell/rna-transcription/src/DNA.hs
@@ -4,16 +4,15 @@ module DNA
 
 import Data.Either.Combinators (mapRight)
 import qualified Data.Map.Strict as Map
-import Data.Maybe (fromJust)
 
 toRNA :: String -> Either Char String
-toRNA = toRNA' (Map.fromList assoc)
+toRNA = toRNA' dnaFrom
   where
-    assoc = [('G', 'C'), ('C', 'G'), ('T', 'A'), ('A', 'U')]
+    dnaFrom = Map.fromList [('G', 'C'), ('C', 'G'), ('T', 'A'), ('A', 'U')]
 
 toRNA' :: Map.Map Char Char -> String -> Either Char String
 toRNA' _ "" = Right ""
-toRNA' dnaFrom (x:xs)
-  | Map.notMember x dnaFrom = Left x
-  | otherwise =
-    mapRight ((fromJust $ Map.lookup x dnaFrom) :) $ toRNA' dnaFrom xs
+toRNA' dnaFrom (x:xs) =
+  case Map.lookup x dnaFrom of
+    Nothing -> Left x
+    Just c -> mapRight (c :) $ toRNA' dnaFrom xs
