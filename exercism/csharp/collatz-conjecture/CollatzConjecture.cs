2973e94bf9676add2c566ad6e603edb4c1553b49
kevin meyers <kevinm1776@gmail.com>
2020-05-14 13:44:37 -0400

    Finished Collatz.

---
+++ exercism/csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    private static int Recur(int n, int s)
+    {
+        if (n <= 1) return s;
+        return n % 2 == 0 ? Recur(n/2, s + 1) : Recur(3*n + 1, s + 1);
+    }
+    public static int Steps(int number)
+    {
+        if (number < 1) throw new ArgumentOutOfRangeException();
+        return Recur(number, 0);
+    }
+}
